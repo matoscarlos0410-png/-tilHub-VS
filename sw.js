7f09130485b979807b96ba70731cecdc0b16cafe
matoscarlos0410-png <matoscarlos0410@gmail.com>
2026-09-19 21:42:06 -0500

    Refactor service worker event listeners and caching
    const CACHE = "utilhub-v14-suprime-master-nova-1";
    
    const CORE_FILES = [
      "./",
      "./index.html",
      "./style.css",
      "./script.js",
      "./manifest.webmanifest",
      "./icon.svg"
    ];
    
    
    self.addEventListener("install", event => {
    
      event.waitUntil(
    
        caches
          .open(CACHE)
          .then(cache =>
            cache.addAll(CORE_FILES)
          )
          .then(() =>
            self.skipWaiting()
          )
    
      );
    
    });
    
    
    self.addEventListener("activate", event => {
    
      event.waitUntil(
    
        caches
          .keys()
          .then(keys =>
    
            Promise.all(
    
              keys
                .filter(key => key !== CACHE)
                .map(key =>
                  caches.delete(key)
                )
    
            )
    
          )
          .then(() =>
            self.clients.claim()
          )
    
      );
    
    });
    
    
    self.addEventListener("fetch", event => {
    
      if (event.request.method !== "GET") {
        return;
      }
    
      const url =
        new URL(event.request.url);
    
      if (
        url.origin !== self.location.origin
      ) {
        return;
      }
    
      event.respondWith(
    
        caches
          .match(event.request)
          .then(cached => {
    
            if (cached) {
              return cached;
            }
    
            return fetch(event.request)
              .then(response => {
    
                if (response.ok) {
    
                  const copy =
                    response.clone();
    
                  caches
                    .open(CACHE)
                    .then(cache => {
    
                      cache.put(
                        event.request,
                        copy
                      );
    
                    });
    
                }
    
                return response;
    
              })
              .catch(() =>
                caches.match("./index.html")
              );
    
          })
    
      );
    
    });

--- sw.js
+++ sw.js
@@ -1,184 +1,116 @@
-const CACHE =
-  "utilhub-v14-suprime-master-nova-1";
-
+const CACHE = "utilhub-v14-suprime-master-nova-1";
 
 const CORE_FILES = [
-
   "./",
-
   "./index.html",
-
   "./style.css",
-
   "./script.js",
-
   "./manifest.webmanifest",
-
   "./icon.svg"
-
 ];
 
 
-self.addEventListener(
-  "install",
-  event => {
+self.addEventListener("install", event => {
 
-    event.waitUntil(
+  event.waitUntil(
 
-      caches
-        .open(CACHE)
-        .then(cache => {
+    caches
+      .open(CACHE)
+      .then(cache =>
+        cache.addAll(CORE_FILES)
+      )
+      .then(() =>
+        self.skipWaiting()
+      )
 
-          return cache.addAll(
-            CORE_FILES
-          );
+  );
 
-        })
-        .then(() => {
-
-          return self.skipWaiting();
-
-        })
-
-    );
-
-  }
-);
+});
 
 
-self.addEventListener(
-  "activate",
-  event => {
+self.addEventListener("activate", event => {
 
-    event.waitUntil(
+  event.waitUntil(
 
-      caches
-        .keys()
-        .then(keys => {
+    caches
+      .keys()
+      .then(keys =>
 
-          return Promise.all(
+        Promise.all(
 
-            keys
+          keys
+            .filter(key => key !== CACHE)
+            .map(key =>
+              caches.delete(key)
+            )
 
-              .filter(
-                key =>
-                  key !== CACHE
-              )
-
-              .map(
-                key =>
-                  caches.delete(key)
-              )
-
-          );
-
-        })
-
-        .then(() => {
-
-          return self.clients.claim();
-
-        })
-
-    );
-
-  }
-);
-
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    if (
-      event.request.method !==
-      "GET"
-    ) {
-
-      return;
-
-    }
-
-
-    const url =
-      new URL(
-        event.request.url
-      );
-
-
-    /*
-      Solo interceptamos archivos
-      del propio sitio.
-    */
-
-    if (
-      url.origin !==
-      self.location.origin
-    ) {
-
-      return;
-
-    }
-
-
-    event.respondWith(
-
-      caches
-        .match(
-          event.request
         )
-        .then(cached => {
 
-          if (cached) {
+      )
+      .then(() =>
+        self.clients.claim()
+      )
 
-            return cached;
+  );
 
-          }
-
-
-          return fetch(
-            event.request
-          )
-
-            .then(response => {
-
-              if (
-                response.ok
-              ) {
-
-                const copy =
-                  response.clone();
+});
 
 
-                caches
-                  .open(CACHE)
-                  .then(cache => {
+self.addEventListener("fetch", event => {
 
-                    cache.put(
-                      event.request,
-                      copy
-                    );
-
-                  });
-
-              }
-
-
-              return response;
-
-            })
-
-            .catch(() => {
-
-              return caches.match(
-                "./index.html"
-              );
-
-            });
-
-        })
-
-    );
-
+  if (event.request.method !== "GET") {
+    return;
   }
-);
+
+  const url =
+    new URL(event.request.url);
+
+  if (
+    url.origin !== self.location.origin
+  ) {
+    return;
+  }
+
+  event.respondWith(
+
+    caches
+      .match(event.request)
+      .then(cached => {
+
+        if (cached) {
+          return cached;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+
+            if (response.ok) {
+
+              const copy =
+                response.clone();
+
+              caches
+                .open(CACHE)
+                .then(cache => {
+
+                  cache.put(
+                    event.request,
+                    copy
+                  );
+
+                });
+
+            }
+
+            return response;
+
+          })
+          .catch(() =>
+            caches.match("./index.html")
+          );
+
+      })
+
+  );
+
+});
